767ed194dc8c99c8724a8eb1f420f74f9f789e9a
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-01-01 22:12:12 +0530

    C++

---
+++ varcomm.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Single line comment in C++
+    /*Multi
+    line comment
+    in C++*/
+    // Variables in C++
+    int a = 6;
+    cout << a << "\n";
+    char b = 'a';
+    cout << b << "\n";
+    float c = 1.234;
+    cout << c << "\n";
+    bool f = false;
+    cout << f << "\n";
+    double z = 3.3436489289239598;
+    cout << z << "\n";
+    return 0;
+}
