e0a86f04033f3bd4bcdb6e1d98088af9b6fdab58
NAVNEET KHARB <145402186+NAVNEET-KHARB@users.noreply.github.com>
2024-01-26 20:53:19 +0530

    Inheritance

---
+++ Inheritance.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+/*
+->Inheritance -> The capability of a class to derive properties and characteristics from another class is called Inheritance.
+->Inheritance is one of the most important features of Object-Oriented Programming.
+->Inheritance is a feature or a process in which, new classes are created from the existing classes.
+->The new class created is called “derived class” or “child class” and the existing class is known as the “base class” or “parent class”.
+->The derived class now is said to be inherited from the base class.
+->When we say derived class inherits the base class, it means, the derived class inherits all the properties of the base class, without changing the properties of base class and may add new features to its own.
+->These new features in the derived class will not affect the base class. The derived class is the specialized class for the base class.
+->Sub Class: The class that inherits properties from another class is called Subclass or Derived Class.
+->Super Class: The class whose properties are inherited by a subclass is called Base Class or Superclass.
+
+Syntax ->
+class  <derived_class_name> : <access-specifier> <base_class_name>
+{
+        //body
+}
+*/
+// Base class
+class Base
+{
+public:
+    // This will also execute for derived class.
+    Base()
+    {
+        cout << "This is a base class." << endl;
+    }
+};
+// Derived Class of Base
+class Derived : Base
+{
+public:
+    Derived()
+    {
+        cout << "This is a derived class." << endl;
+    }
+};
+int main()
+{
+    Base b;
+    Derived d;
+    return 0;
+}
